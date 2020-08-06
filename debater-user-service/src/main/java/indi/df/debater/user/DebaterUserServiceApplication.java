f6aef117b91b093e52f146f19f4aa2ca15a98841
dingfan <dingfanim@163.com>
2020-08-06 18:51:18 +0800

    2020.8.6

---
+++ debater-user-service/src/main/java/indi/df/debater/user/DebaterUserServiceApplication.java
@@ -0,0 +1,13 @@
+package indi.df.debater.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebaterUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebaterUserServiceApplication.class, args);
+    }
+
+}
